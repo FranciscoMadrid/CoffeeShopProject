512ece14a26bfc5057b31f8f4a0b7d9a29b3e30e
FranciscoMadrid <ffms-a@outlook.com>
2021-03-25 11:15:24 -0600

    Form de cliente WIP

---
+++ CoffeeShopProject/Clientes.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+
+namespace proyecto
+{
+    /// <summary>
+    /// Interaction logic for Clientes.xaml
+    /// </summary>
+    public partial class Clientes : Window
+    {
+        public Clientes()
+        {
+            InitializeComponent();
+        }
+
+        private void btningresar_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void btnmodificar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
