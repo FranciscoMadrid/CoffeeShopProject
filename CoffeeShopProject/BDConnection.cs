32fd75e3b6c21010b33de24e13145a79a78ebd30
khaleb <khaleb@DESKTOP-OA2OCU3>
2021-03-29 09:31:23 -0600

    se integra DBConnection al modulo empleados

---
+++ CoffeeShopProject/BDConnection.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Windows;
+
+namespace CoffeeShopProject
+{
+    class BDConnection
+    {
+        private static string SQLConnectionString = @"server = (local)\SQLEXPRESS; Initial Catalog = CoffeeShopDB; Integrated Security = True";
+
+        protected SqlConnection sqlConnection = new SqlConnection(SQLConnectionString);
+
+        public BDConnection()
+        {
+
+        }
+
+        public void CheckConnection()
+        {
+            try
+            {
+                sqlConnection.Open();
+                MessageBox.Show("Estas connectado a " + sqlConnection.Database.ToString() + " exitosamente!!");
+            }
+            catch (Exception ex)
+            {
+                sqlConnection.Close();
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
+        }
+
+    }
+}
