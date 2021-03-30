898bad455034411e201eca0427ee4604a4c75269
Edgardo Rojas <erojas504@outlook.com>
2021-03-30 16:51:55 -0600

    agrego la bd de mi proyecto a mi rama personal, y ademas la creacion de la clase para la cadena de conexion

---
+++ CoffeeShopProject/BDConnection.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace CoffeeShopProject
+{
+    class BDConnection
+    {
+        private static string SQLConnectionString = @"server = (local)";
+
+        protected SqlConnection sqlConnection = new SqlConnection (SQLConnectionString);
+
+        public BDConnection()
+        {
+
+        }
+
+        public void CheckConnection() 
+        {
+            try
+            {
+                sqlConnection.Open();
+            }
+            catch (Exception ex)
+            {
+                sqlConnection.Close();
+                Console.WriteLine("Error: {0} {1}", ex.Message, ex.StackTrace);
+
+
+            }
+
+            finally 
+            {
+                sqlConnection.Close();
+            }
+        }
+
+
+    }
+}
