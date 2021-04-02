bbdb79fb631d4ddb9a40b6c95303fb889e9f8ad0
khaleb <khaleb@DESKTOP-OA2OCU3>
2021-04-02 13:17:39 -0600

    Se crea el formulario para agregar usuarios de para la administracion de el sistema

---
+++ CoffeeShopProject/frmNuevoUsuario.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CoffeeShopProject
+{
+    /// <summary>
+    /// Interaction logic for frmNuevoUsuario.xaml
+    /// </summary>
+    public partial class frmNuevoUsuario : Window
+    {
+        public frmNuevoUsuario()
+        {
+            InitializeComponent();
+            cmbtipousuario.Items.Add("Administrativo");
+            cmbtipousuario.Items.Add("Vendedor");
+
+        }
+    }
+}
