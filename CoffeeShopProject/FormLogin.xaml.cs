15ca23e57ce76237f112e080d7899d800837ff63
FranciscoMadrid <ffms-a@outlook.com>
2021-04-16 10:53:23 -0600

    *Funcionalidad de login

---
+++ CoffeeShopProject/FormLogin.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CoffeeShopProject
+{
+    /// <summary>
+    /// Interaction logic for FormLogin.xaml
+    /// </summary>
+    public partial class FormLogin : Window
+    {
+        Logins logins = new Logins();
+        int EmpleadoId = 0;
+        int TipoUsuario = 0;
+        public FormLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void txtUsuario_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void btnLogging_Click(object sender, RoutedEventArgs e)
+        {
+            if(CheckLoginData())
+            {
+                GetLoginData();
+                if(CheckEmpleadoID(logins.CheckCuenta()))
+                {
+                    MessageBox.Show("Ingreso exitoso!");
+                }
+                else
+                {
+                    MessageBox.Show("El usuario o la contraseña ingresada estan incorrectas.");
+                }
+                Clear();
+            }
+        }
+
+        private bool CheckEmpleadoID (int id)
+        {
+            if (id != 0)
+                return true;
+            else
+                return false;
+        }
+
+        private bool CheckLoginData ()
+        {
+            if (string.IsNullOrEmpty(txtUsuario.Text) || string.IsNullOrEmpty(txtContrasena.Text))
+            {
+                MessageBox.Show("Por favor, llene todas las casillas.");
+                return false;
+            }
+            else
+                return true;
+        }
+
+        private void GetLoginData ()
+        {
+            logins.Usuario = txtUsuario.Text;
+            logins.Contrasena = txtContrasena.Text;
+        }
+
+        private void btnSalir_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Clear()
+        {
+            txtContrasena.Clear();
+            txtUsuario.Clear();
+        }
+    }
+}
---
+++ CoffeeShopProject/Logins.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows;
+
+namespace CoffeeShopProject
+{
+    class Logins : EmpleadosUsuarios
+    {
+        public Logins()
+        {
+        }
+
+        public int CheckCuenta()
+        {
+            int EmpleadoID = 0;
+            try
+            {
+                string spNombre = @"[dbo].[sp_Empleados_EmpleadosUsuarios_Checker]";
+
+                SqlCommand cmd = new SqlCommand(spNombre, sqlConnection);
+
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Usuario", Usuario);
+                cmd.Parameters.AddWithValue("@Contrasena", Contrasena);
+
+                cmd.Parameters.Add("@EmpleadoId", SqlDbType.Int);
+                cmd.Parameters["@EmpleadoId"].Direction = ParameterDirection.Output;
+
+                sqlConnection.Open();
+                cmd.ExecuteNonQuery();
+
+                EmpleadoID = int.Parse(cmd.Parameters["@EmpleadoId"].Value.ToString());
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
+            return EmpleadoID;
+        }
+    }
+}
